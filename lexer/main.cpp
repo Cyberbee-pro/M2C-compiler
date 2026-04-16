93729efdc650df915853fe15b140cf0df68d5082
Cyberbee-pro <cyberbeepro@gmail.com>
2026-04-16 15:29:11 +0530

    initial lexer , enums

--- lexer/main.cpp
+++ lexer/main.cpp
@@ -1,10 +1,35 @@
 #include<iostream>
+#include<bits/stdc++.h>
+#include<string>
 
-// enum TokenKey{
-    // 
-// };
+
+enum TokenKey{
+    MAIN,
+    FOR,
+    WHILE,
+    IF,
+    PRINT,
+    SCAN
+};
+
+enum TokenSeparator{
+    CURL_ST,
+    COMA,
+    SEMI_COLLON,
+};
 
 int main (){
-    std::cout<<"Hi Mom";
+    std::cout<<"Hi Mom"<<std::endl;
+    std::ifstream inputFile("test.cym2c");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open the file!" << std::endl;
+        return 1;
+    }
+    std::string readLine;
+    // 3. Read the file line by line
+    while (std::getline(inputFile, readLine)) {
+        std::cout << readLine << std::endl;
+    }
+
     return 0;
 }
