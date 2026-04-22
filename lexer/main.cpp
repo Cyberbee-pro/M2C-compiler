30854591104e739d618db1dc0ca28276294ad0dc
Cyberbee-pro <cyberbeepro@gmail.com>
2026-04-23 02:57:31 +0530

    now has more sofisticated and robust fileReader::readFile()

--- lexer/main.cpp
+++ lexer/main.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
-#include<bits/stdc++.h>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <locale>
 
-
-enum TokenKey{
+enum TokenKey
+{
     MAIN,
     FOR,
     WHILE,
@@ -13,85 +15,120 @@ enum TokenKey{
     SCAN
 };
 
-enum TokenSeparator{
+enum TokenSeparator
+{
     CURL_ST,
     COMA,
     SEMI_COLLON,
     SPACE
 };
 
-class fileReader{
-    private:
+class fileReader
+{
+private:
     std::ifstream inputFile;
     std::string fileName, readLine;
     int i;
 
-    public:
-
-    fileReader(){
-        
+public:
+    fileReader()
+    {
     }
 
-    fileReader(std::string fileName){
+    fileReader(std::string fileName)
+    {
         this->fileName = fileName;
         openFile(1);
     }
 
     // std::ifstream inputFile("test.cym2c");
-    void openFile(){
-        std::cout<<"\nEnter file name:"<<std::endl;
-        std::cin>>fileName;
+    void openFile()
+    {
+        std::cout << "\nEnter file name:" << std::endl;
+        std::cin >> fileName;
         inputFile.open(fileName);
-        if (!inputFile.is_open()) {
+        if (!inputFile.is_open())
+        {
             std::cerr << "Error: Could not open the file!" << std::endl;
             exit(1);
         }
     }
 
-    void openFile(int i){
+    void openFile(int i)
+    {
         inputFile.open(fileName);
-        if (!inputFile.is_open()) {
+        if (!inputFile.is_open())
+        {
             std::cerr << "Error: Could not open the file!" << std::endl;
             exit(1);
         }
     }
 
     // 3. Read the file line by line
-    void readFile(){
-        while (std::getline(inputFile, readLine)) {
-            // std::cout << readLine << std::endl;
+    void readFile()
+    {
+        while (std::getline(inputFile, readLine))
+        {
+            // 4. Tokenize each line into tokens and separators
             std::string buffer = "";
-            for(i = 0 ; i < readLine.length() ; i++){
-                
-                if(readLine[i] == ' ' ||readLine[i] == ';'){
-                    std::cout<<"\n Seperator : \""<< readLine[i]<<"\""<<std::endl;
-                    if(buffer!=""){
-                        std::cout<<"\n Buffer : "<<buffer<<std::endl;
-                        buffer="";
+            for (i = 0; i < readLine.length(); i++)
+            {
+                // Checks for separators ie space and semicolon
+                if (readLine[i] == ' ' || readLine[i] == ';')
+                {
+                    std::cout << "\nSeperator : \"" << readLine[i] << "\"" << std::endl;
+                    if (buffer != "")
+                    {
+                        std::cout << "Buffer : " << buffer <<std::endl<< std::endl;
+                        buffer = "";
                     }
-                }else{
-                        buffer+=readLine[i];
-                        std::cout<<"\n Token : "<< readLine[i]<<std::endl;
-                    }
-            }
-            if(i==readLine.length()&&buffer!=""){
-                    std::cout<<"\n Buffer : "<<buffer<<std::endl;
-                    buffer=""; 
                 }
-        
+                // checks for curly braces
+                else if (readLine[i] == '{' || readLine[i] == '}')
+                {
+                    std::cout << "\nSeperator : \"" << readLine[i] << "\"" << std::endl;
+                }
+                // checks for number tokens
+                else if (std::isdigit(readLine[i]))
+                {
+                    buffer += readLine[i];
+                    std::cout << "Token_Numeric : " << readLine[i] << std::endl;
+                }
+                // checks for alphabetic tokens
+                else if (std::isalpha(readLine[i]))
+                {
+                    buffer += readLine[i];
+                    std::cout << "Token_Alphabetic : " << readLine[i] << std::endl;
+                }
+                // prints the token if it is not a separator and adds it to the buffer
+                else
+                {
+                    buffer += readLine[i];
+                    std::cout << "Token : " << readLine[i] << std::endl;
+                }
+                
+            }
+            // throws error if line ends without separator and buffer is not empty
+            if (i == readLine.length() && buffer != "")
+            {
+                std::cout << "\n Buffer : " << buffer << std::endl;
+                std::cout << "ERROR : Line end without separator!!" << std::endl;
+                buffer = "";
+                std::cout << "Line end" << std::endl;
+            }
         }
     }
 
-    ~fileReader(){
-        std::cout<<"\ndestructor called!!"<<std::endl;
+    ~fileReader()
+    {
+        std::cout << "\ndestructor called!!" << std::endl;
         inputFile.close();
     }
-
 };
 
-
-int main (){
-    std::cout<<"Hi Mom"<<std::endl;
+int main()
+{
+    // std::cout << "Hi Mom" << std::endl;
     fileReader file("test.cym2c");
     // file.openFile();
     file.readFile();
