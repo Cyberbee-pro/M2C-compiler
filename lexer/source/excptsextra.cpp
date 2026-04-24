7172e8c45bea977d15b2c8211da86f64b6d58067
Cyberbee-pro <cyberbeepro@gmail.com>
2026-04-25 00:13:29 +0530

    some updates

--- lexer/source/excptsextra.cpp
+++ lexer/source/excptsextra.cpp
@@ -6,7 +6,7 @@ CompileError::CompileError(std::string message, std::string Line, int lineNumber
             this->message = message;
             this->Line = Line;
             this->lineNumber = lineNumber;
-            ErrorMsg = "Compile Error : " + message +" At Line (" + std::to_string(lineNumber) +") : " + Line ;
+            ErrorMsg = "Compile Error : " + message +" At Line (" + std::to_string(lineNumber+1) +") : " + Line ;
         }
 
 const char* CompileError::what() const noexcept {
